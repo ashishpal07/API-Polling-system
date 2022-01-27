e59f873e7e955cc59fed3f5d1a3d4bb015ef14c1
ashishpal07 <ashishpal7861@gmail.com>
2022-01-27 20:14:35 +0530

    completed polling-system on localhost mongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://ashish:ashish123@polling-system.64srs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb://localhost:27017/api-polling');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', () => {
+    console.log('Connected to Database : MongoDB');
+});
+
+module.exports = db;
